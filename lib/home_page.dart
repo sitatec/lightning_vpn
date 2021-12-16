7a969cbd1ce9a4d7fb53417093fe095fd4fd1056
Sita Bérété <sita.berete.3@gmail.com>
2021-12-16 01:04:15 +0000

    finalize home page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -9,13 +9,14 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool _isConnected = false;
+  bool _isConnecting = false;
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
     return Scaffold(
-      backgroundColor: Colors.white,
       appBar: AppBar(
         leading: IconButton(
-          iconSize: 28,
           icon: const Icon(
             Icons.menu,
             color: Colors.blue,
@@ -24,39 +25,170 @@ class _HomePageState extends State<HomePage> {
         ),
         actions: [
           Padding(
-            padding: const EdgeInsets.all(8),
+            padding: const EdgeInsets.all(10),
             child: TextButton(
               onPressed: () {},
-              child: const Text("ADD TIME"),
+              child: const Text(
+                "ADD TIME",
+                textScaleFactor: 0.999,
+              ),
               style: TextButton.styleFrom(
                 shape: RoundedRectangleBorder(
                   side: const BorderSide(
                     width: 0.3,
                     color: Colors.blue,
                   ),
-                  borderRadius: BorderRadius.circular(5),
+                  borderRadius: BorderRadius.circular(3),
                 ),
               ),
             ),
           )
         ],
-        systemOverlayStyle: SystemUiOverlayStyle.dark,
+        systemOverlayStyle:
+            const SystemUiOverlayStyle(statusBarColor: Colors.white),
         shadowColor: Colors.black12,
-        backgroundColor: Colors.white,
+        backgroundColor: theme.scaffoldBackgroundColor,
         title: const Text(
           "Lithning VPN",
-          style: TextStyle(color: Colors.black87),
+          style: TextStyle(color: Colors.black87, fontSize: 19),
         ),
       ),
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/background.png"),
-            // colorFilter: ColorFilter.mode(Colors.white, BlendMode.luminosity),
-            fit: BoxFit.cover,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const InfoRow("IP", "192.54.36.373"),
+            Image.asset("assets/images/logo.png"),
+            // Expanded(
+            //     child: Container(
+            //   child: Image.asset("assets/images/logo.png"),
+            // )),
+            const InfoColumn("Remaining Time", "03:04:30", switchedColor: true),
+            // const SizedBox(height: 32),
+            ListTile(
+              tileColor: Colors.grey[200],
+              leading: const Icon(Icons.language),
+              trailing: const Icon(Icons.arrow_forward_ios_rounded),
+              title: const Text("Fastest Server"),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5),
+              ),
+              onTap: () {},
+            ),
+            // const SizedBox(height: 28),
+            TextButton(
+              style: TextButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  primary: Colors.white,
+                  padding: const EdgeInsets.symmetric(vertical: 14)),
+              onPressed: () => setState(() => _isConnected = !_isConnected),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  if (!_isConnecting)
+                    _isConnected
+                        ? Image.asset(
+                            "assets/images/flash.png",
+                            width: 24,
+                            height: 24,
+                            scale: 25,
+                          )
+                        : const Icon(Icons.wifi_off_rounded),
+                  const SizedBox(width: 8),
+                  _isConnecting
+                      ? const SizedBox(
+                          width: 24,
+                          height: 24,
+                          child: CircularProgressIndicator(
+                            color: Colors.white,
+                            strokeWidth: 3,
+                          ),
+                        )
+                      : Text(
+                          _isConnected ? "CONNECTED" : "DISCONNECTED",
+                          textScaleFactor: 1.3,
+                        ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class InfoRow extends StatelessWidget {
+  final String _label;
+  final String _value;
+  final bool switchedColor;
+  const InfoRow(
+    this._label,
+    this._value, {
+    this.switchedColor = false,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          "$_label: ",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: switchedColor ? Colors.black.withAlpha(180) : Colors.blue,
+            fontSize: 17,
           ),
         ),
-      ),
+        Text(
+          _value,
+          style: TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: 17,
+            color: switchedColor ? Colors.blue : Colors.black.withAlpha(180),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class InfoColumn extends StatelessWidget {
+  final String _label;
+  final String _value;
+  final bool switchedColor;
+  const InfoColumn(
+    this._label,
+    this._value, {
+    this.switchedColor = false,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          "$_label ",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: switchedColor ? Colors.black.withAlpha(180) : Colors.blue,
+            fontSize: 16,
+          ),
+        ),
+        const SizedBox(height: 7),
+        Text(
+          _value,
+          style: TextStyle(
+            fontWeight: FontWeight.w500,
+            fontSize: 18,
+            color: switchedColor ? Colors.blue : Colors.black.withAlpha(180),
+          ),
+        )
+      ],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Lithning VPN',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const SafeArea(child: HomePage()),
+      home: SafeArea(child: HomePage()),
     );
   }
 }
